cd0f2f845fed920a44e0c6db43913fceb4f831e9
lauralandonlovell <roryboo@gmail.com>
2023-01-04 16:34:24 +0100

    syntax problems

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -3,10 +3,7 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("HELLO SANDBOX WORLD!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-    }
-    {   Console.Write('What is your first name?');
-        string name = Console.ReadLine();
-    }
-}
+}   
+Console.WriteLine("HELLO SANDBOX WORLD!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    
+
