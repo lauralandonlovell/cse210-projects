b4ef63ee31e7a29c9dc088413747200d5b59964a
lauralandonlovell <roryboo@gmail.com>
2023-01-04 16:15:53 +0100

    first c# attempt

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,14 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
-    }
+    static void Main(string[] args);
+    
 }
+
+    Console.WriteLine("Hello Prep 1 World!");
+    
+    
+       
+        Console.Write('What is your first name? ');
+        string fname = Console.ReadLine();
+    
