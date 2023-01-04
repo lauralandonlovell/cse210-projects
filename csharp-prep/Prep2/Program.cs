998772a5f02b0130a697d016f3f37c5424ab6fa4
lauralandonlovell <roryboo@gmail.com>
2023-01-04 19:51:39 +0100

    completed prep2 still have 2 authors (ME)

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,13 +2,43 @@ using System;
 
 class Program
 {
-static void Main(string[] args)
-    if (x > y)
+    static void Main(string[] args)
     {
-        Console.WriteLine('greater than');
-    }
-    else 
-{   
-        Console.WriteLine('less than');
+        Console.Write('What is your grade percentage? ');
+        string input = Console.ReadLine();
+        int percent = int.Parse(input);
+        string grade = '';
+        
+        if (percent >= 90)
+        {
+            grade = 'A';
+        }
+        else if (percent >= 80)
+        {
+            grade = 'B';
+        }
+        else if (percent >= 70)
+        {
+            grade = 'C';
+        }
+        else if (percent >= 60)
+        {
+            grade = 'D';
+        }
+        else
+        {
+            grade = 'F';
+        }
 
+        Console.WriteLine($'Your grade is: {grade}.');
+
+        if (percent >= 70)
+        {
+            Console.WriteLine('You passed!!!!!');
+        }
+        else
+        {
+            Console.WriteLine('Keep Trying!');
+        }
+    }
 }
