7cfb70624fa83e2888490fb52ea608a7d8d08e9a
lauralandonlovell <roryboo@gmail.com>
2023-01-05 20:21:00 +0100

    created name function

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.WriteLine("Hello Prep 5 World!");
+        
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName(string first, string last)
+    {
+        Console.Write("What is your first name? ");
+        string fname = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lname = Console.ReadLine();
+
+        string name = ($"{fname} {lname}");
+        return name;
     }
 }
