4a30be8655c2e9cc6bd4b1310d4ea4d25dc5b2a3
lauralandonlovell <roryboo@gmail.com>
2023-01-05 15:39:24 +0100

    trying while loop

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,25 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        
-        Console.WriteLine("What is the magic number?");
         int mnumber = 6;
+        int guess = -1;
+        while (guess != mnumber)
+        {
+            Console.WriteLine("What is the magic number?");
+            Console.WriteLine(mnumber);
         
-        Console.Write("What is your guess? ");
-        int guess = Convert.ToInt32(Console.ReadLine());
-
-        if (guess < mnumber)
-        {
-            Console.WriteLine("Higher!");
+            Console.Write("What is your guess? ");
+            int guess = Convert.ToInt32(Console.ReadLine());
+        
+            if (guess < mnumber)
+            {
+                Console.WriteLine("Higher!");
+            }
+            else if (guess > mnumber)
+            {
+                Console.WriteLine("Lower!");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
         }
-        else if (guess > mnumber)
-        {
-            Console.WriteLine("Lower!");
-        }
-        else
-        {
-            Console.WriteLine("You guessed it!");
-        }
+        
         
     }
     
