380da8ac85fa6778c9f8837128effcc3fbf27eb3
lauralandonlovell <roryboo@gmail.com>
2023-01-05 19:32:44 +0100

    adding loop and creating list

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,25 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> nums = new List<int>();
+
+        int input = -1;
+        while (input != 0)
+        {
+            Console.Write("Enter numbers and type 0 to quit: ");
+
+            string userInput = Console.ReadLine();
+            input = int.Parse(userInput);
+
+            if (input != 0)
+            {
+                nums.Add(input);
+            }
+        }
+        
     }
 }
