daf6b531f98694e5f1d69d6daa36d4289ca12b20
lauralandonlovell <roryboo@gmail.com>
2023-01-06 17:10:05 +0100

    forgot class when creating instance

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,13 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        job1 = new Job();
+        Job job1 = new Job();
         job1._jobTitle = "Web Page Administrator";
         job1._company = "Clydesdale Horse Farm";
         job1._startYear = "2019";
         job1._endYear = "2023";
 
-        job2 = new Job();
+        Job job2 = new Job();
         job2._company = "Amazing Baking";
         job2._jobTitle = "Baker";
         job2._startYear = "2021";
