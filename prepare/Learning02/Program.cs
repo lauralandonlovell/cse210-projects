db145e902b121375d8bd870a6d245e9bb1c29b49
lauralandonlovell <roryboo@gmail.com>
2023-01-06 16:52:47 +0100

    added datatype before variable

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,18 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        job1 = new Job();
-        job1._jobTitle = "Web Page Administrator";
-        job1._company = "Clydesdale Horse Farm";
-        job1._startYear = "2019";
-        job1._endYear = "2023";
+        string job1 = new Job();
+        string job1._jobTitle = "Web Page Administrator";
+        string job1._company = "Clydesdale Horse Farm";
+        int job1._startYear = "2019";
+        int job1._endYear = "2023";
 
-        job2 = new Job();
-        job2._company = "Amazing Baking";
-        job2._jobTitle = "Baker";
-        job2._startYear = "2021";
-        job2._endYear = "2023";
+        string job2 = new Job();
+        string job2._company = "Amazing Baking";
+        string job2._jobTitle = "Baker";
+        int job2._startYear = "2021";
+        int job2._endYear = "2023";
 
-        Console.WriteLine($"Job Title: {job1._jobTitle} ({job1._company}))
+        Console.WriteLine($"Job Title: {job1._jobTitle} Company:({job1._company}) Duration: {job1._startYear} - {job1._endYear}.");
     }
 }
