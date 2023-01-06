a9d6e10cab55d2a6749fa84c06f73844392931de
lauralandonlovell <roryboo@gmail.com>
2023-01-06 16:33:39 +0100

    creating job class

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Job
+{
+    public string _company = "";
+    public string _jobTitle = "";
+    public string _startYear = "";
+    public string _endYear = "";
+
+    public void Display()
+    {
+        Console.WriteLine($" I have experience in the role of {_jobTitle} working for {_company} from {_startYear} until {_endYear}.");
+    }
+
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        job1 = new Job();
+        job1._company = "Clydesdale Horse Farm";
+        job1._jobTitle = "Web Page Administrator";
+        job1._startYear = "2019";
+        job1._endYear = "2023";
+
+        job2 = new Job();
+        job2._company = "Amazing Baking";
+        job2._jobTitle = "Baker";
+        job2._startYear = "2021";
+        job2._endYear = "2023";
     }
 }
