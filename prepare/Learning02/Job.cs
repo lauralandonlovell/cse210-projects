09ee3ad24cc014f2aa0b457f0373858a6cf15a16
lauralandonlovell <roryboo@gmail.com>
2023-01-13 18:19:15 +0100

    creating Job class

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -2,20 +2,5 @@ using System;
 
 public class Job
 {
-    string _jobTitle = "Receptionist";
-    string _company = "";
-    string _startYear = "";
-    string _endYear = "";
-
-    static void Main(string [] args)
-    {
-        
-    }
-    
-
-    // public void Display()
-    // {
-        // Console.WriteLine($" I have experience in the role of {_jobTitle} working for {_company} from {_startYear} until {_endYear}.");
-    // }
-
+    string jobtitle = "Receptionist";
 }
