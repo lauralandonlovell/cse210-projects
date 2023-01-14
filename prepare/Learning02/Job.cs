767e1a614037d54561d331d5deac8dd37ca070dd
lauralandonlovell <roryboo@gmail.com>
2023-01-14 13:54:28 +0100

    has errors

--- prepare/Learning02/Job.cs
+++ prepare/Learning02/Job.cs
@@ -2,5 +2,14 @@ using System;
 
 public class Job
 {
-    string jobtitle = "Receptionist";
+    public string _jobTitle;
+    public string _company;
+    public string _startYear;
+    public string _endYear;
+
+    public void Display()
+    {
+        Console.WriteLine($" I have experience in the role of {_jobTitle} working for {_company} from {_startYear} until {_endYear}.");
+    }
+
 }
