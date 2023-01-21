e2382449e0560db4c0148ff6f5f6300ec79c4424
lauralandonlovell <roryboo@gmail.com>
2023-01-21 19:14:12 +0100

    stuck on list

---
+++ prepare/Learning03/Menu.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Collections.Generic;
+public class Menu
+{
+    public List<string> ChoicesList = new List<string>();
+    ChoicesList.Add
+
+}   
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -3,25 +3,28 @@ using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args) {
-
-        //int i = 1;
-        //while (i<=6)
-        {
-            Console.WriteLine("C# For Loop: Iteration {0}, i");
+//reading https://www.c-sharpcorner.com/UploadFile/75a48f/list-collection-class-in-C-Sharp/ and https://www.c-sharpcorner.com/UploadFile/mahesh/create-a-list-in-C-Sharp/  helped me create new list
+        int i = 1;
+        while (i<=6)
+        {   
             Console.WriteLine("Keep track of your day! Choose one of the following actions:");
-            Console.WriteLine("1. LunchBreak Entry");
-            Console.WriteLine("2. Write");
-            Console.WriteLine("3. Display");
-            Console.WriteLine("4. Read");
-            Console.WriteLine("5. Save");
-            Console.WriteLine("6. Quit");
-            Console.Write("What would you like to do?");
-            let string response = Console.ReadLine();
-            
-            if (response == "1")
-            {
-               class 
+            //List<string> MenuChoices = new List<string>();
+            {   
+
             }
+            // Console.WriteLine("1. LunchBreak Entry");
+            // Console.WriteLine("2. Write");
+            // Console.WriteLine("3. Display");
+            // Console.WriteLine("4. Read");
+            // Console.WriteLine("5. Save");
+            // Console.WriteLine("6. Quit");
+            // Console.WriteLine("What would you like to do?");
+            // let string response = Console.ReadLine();
+            
+            // if (response == "1")
+            // {
+            //    class 
+            // }
             // {
             //     break;
             // }
