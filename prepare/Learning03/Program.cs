09465ce983b4c959a10cb0fed981427fbae8111f
lauralandonlovell <roryboo@gmail.com>
2023-01-21 16:46:29 +0100

    don't think I need loop

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,8 +4,8 @@ class Program
 {
     static void Main(string[] args) {
 
-        int i = 1;
-        while (i<=5)
+        //int i = 1;
+        //while (i<=6)
         {
             Console.WriteLine("C# For Loop: Iteration {0}, i");
             Console.WriteLine("Keep track of your day! Choose one of the following actions:");
@@ -16,8 +16,13 @@ class Program
             Console.WriteLine("5. Save");
             Console.WriteLine("6. Quit");
             Console.Write("What would you like to do?");
-            Console.ReadLine();
-            i++;
+            let string response = Console.ReadLine();
+            
+            if (response  = "1")
+            // {
+            //     break;
+            // }
+            //i++;
         }
         
     }
