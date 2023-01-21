a0b0ad526f867e5a14a509c185418246f9d8f140
lauralandonlovell <roryboo@gmail.com>
2023-01-21 15:18:58 +0100

    for while loop working

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args) {
-        Console.WriteLine("Keep track of your day! Choose one of the following actions:");
-        Console.WriteLine("1. LunchBreak Entry");
-        Console.WriteLine("2. Write");
-        Console.WriteLine("3. Display");
-        Console.WriteLine("4. Read");
-        Console.WriteLine("5. Save");
-        Console.WriteLine("6. Quit");
 
+        int i = 1;
+        while (i<=5)
+        {
+            Console.WriteLine("C# For Loop: Iteration {0}, i");
+            Console.WriteLine("Keep track of your day! Choose one of the following actions:");
+            Console.WriteLine("1. LunchBreak Entry");
+            Console.WriteLine("2. Write");
+            Console.WriteLine("3. Display");
+            Console.WriteLine("4. Read");
+            Console.WriteLine("5. Save");
+            Console.WriteLine("6. Quit");
+            i++;
+        }
         
     }
     
