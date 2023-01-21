cbe204fb987bfe151b90c6b797b87e986480481c
lauralandonlovell <roryboo@gmail.com>
2023-01-21 07:17:45 +0100

    reinstalled .NET 7.0.101.

--- prepare/Learning03/Journal.cs
+++ prepare/Learning03/Journal.cs
@@ -1,6 +1,1 @@
-using System;
 
-public class Journal
-{
-    _entries <Entry>
-}
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -3,13 +3,5 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Entry entry1 = new Entry();
-        entry1._place = Console.ReadLine();
-        entry1._mood = Console.ReadLine();
-        entry1._dayStart = Console.ReadLine();
-        entry1._dayEnd = Console.ReadLine();
-
-
-    }
+    
 }
