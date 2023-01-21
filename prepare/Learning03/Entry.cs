4cfdf41ab2b467300f5820c3b8f3b4d9ab3176e6
lauralandonlovell <roryboo@gmail.com>
2023-01-21 13:02:17 +0100

    -DateTime method.Puting in main.

--- prepare/Learning03/Entry.cs
+++ prepare/Learning03/Entry.cs
@@ -10,13 +10,12 @@ class Entry
     public string _weather;
     public string _season;
     public string _scenery;
-    public string _thought;
-
-    public string 
+    public string _thought; 
 
     public void Display() {
         
-        Console.WriteLine($"Today is {_date}. I am writing from {_place}. I am in a {_mood} mood. I had a {_dayStart} to my day. I met {_people} today. The weather was {_weather}. The season has been {_season} this year. I am surrounded by {_scenery}. I have been thinking a lot about {_thought}.");
+        Console.WriteLine("Today is {_date}.");
+        Console.WriteLine($" I am writing from {_place}. I am in a {_mood} mood. I had a {_dayStart} to my day. I met {_people} today. The weather was {_weather}. The season has been {_season} this year. I am surrounded by {_scenery}. I have been thinking a lot about {_thought}.");
 
     }
 
