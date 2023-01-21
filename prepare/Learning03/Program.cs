f53c6e7533a5a543f40b7e259fe271d7f2d8ce49
lauralandonlovell <roryboo@gmail.com>
2023-01-21 14:59:25 +0100

    writing menu

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,17 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args) {
         Console.WriteLine("Keep track of your day! Choose one of the following actions:");
+        Console.WriteLine("1. LunchBreak Entry");
+        Console.WriteLine("2. Write");
+        Console.WriteLine("3. Display");
+        Console.WriteLine("4. Read");
+        Console.WriteLine("5. Save");
+        Console.WriteLine("6. Quit");
+
+        
     }
     
 }
