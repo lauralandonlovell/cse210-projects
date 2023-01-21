cbec61f80a055222e6dcab57c14a2f147261d274
lauralandonlovell <roryboo@gmail.com>
2023-01-21 09:10:44 +0100

    uninstalled latest version of .NET

---
+++ prepare/Learning03/Entry.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Entry
+{
+    public string _place;
+    public string _mood;
+    public string _dayStart;
+    public string _people;
+    public string _weather;
+    public string _season;
+    public string _scenery;
+    public string _thought;
+
+    public void Display() {
+        
+    }
+
+}
