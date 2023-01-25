10584daf4e77f4f065efe2c09b1b4842974e0ebc
lauralandonlovell <roryboo@gmail.com>
2023-01-25 08:27:18 +0100

    commenting out

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -8,11 +8,11 @@ class Program
     static void Main(string[] args) 
     {
 /*reading https://www.c-sharpcorner.com/UploadFile/75a48f/list-collection-class-in-C-Sharp/ and https://www.c-sharpcorner.com/UploadFile/mahesh/create-a-list-in-C-Sharp/  helped me create new list*/
-        int i = 1;
-        while (i <= 6)
+        // int i = 1;
+        // while (i <= 6)
 
         {
-            string input;
+            // string input;
             Console.WriteLine("Welcome to the Journal Program. Enjoy keeping track of your day!");
             string[] menuOptions = {
                 "1. LunchBreak Entry",
@@ -28,7 +28,7 @@ class Program
                 Console.WriteLine(option);
             }
             Console.WriteLine("Please enter a number from the menu above:");
-            input = Console.ReadLine();//switch statement programiz.com
+            //input = Console.ReadLine();//switch statement programiz.com
 
             // if (input != "1")
             // {
